959e40b88346e27032a45003a8018f048af51fc1
Adam Mark <adammark75@gmail.com>
2012-01-19 15:45:35 -0500

    Removed play count

--- media-queries/js/app.js
+++ media-queries/js/app.js
@@ -7,13 +7,3 @@ Mark.pipes.runtime = function (time, factor) {
     var s = Math.floor((time / (factor || 1)) % 60);
     return m + ":" + ("00" + s).substr(-2);
 };
-
-Mark.pipes.numberformat = function (num, precision, signed) {
-    var m = (+num).toFixed(precision).match(/\d+/g),
-        i = m[0].length % 3,
-        d = m[1] ? "." + m[1] : "",
-        a = i ? [m[0].slice(0, i)] : [],
-        s = signed && num > 0 ? "+" : (num < 0) ? "-" : "";
-
-    return s + a.concat(m[0].slice(i).match(/\d{3}/g) || []).join() + d;
-};
