1a233b3ba72c190d3f51f9659067c2227e3f448b
Adam Mark <adammark75@gmail.com>
2012-05-22 10:48:16 -0400

    Moved some methods around

--- parameterized-feed/javascripts/views/forecast.js
+++ parameterized-feed/javascripts/views/forecast.js
@@ -1,20 +1,5 @@
 var WeatherView = function () {
 
-    // handle response from bc.core.getData()
-    var handleData = function (data) {
-        var template = bc.templates["forecast-template"];
-        var context = { "days": data };
-        var elem = document.getElementById("forecast");
-
-        elem.innerHTML = Mark.up(template, context);
-    };
-
-    // handle error from bc.core.getData()
-    var handleError = function (error) {
-        console.log(error);
-    };
-
-    // load weather for the given ZIP code
     this.loadWeather = function (zip) {
         var options = {
             parameterizedFeedValues: {
@@ -25,4 +10,16 @@ var WeatherView = function () {
         bc.core.getData("weather", handleData, handleError, options);
     };
     
+    var handleData = function (data) {
+        var template = bc.templates["forecast-template"];
+        var context = { "days": data };
+        var elem = document.getElementById("forecast");
+
+        elem.innerHTML = Mark.up(template, context);
+    };
+
+    var handleError = function (error) {
+        console.log(error);
+    };
+
 };
