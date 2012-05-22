cf1a1924ad6a14e2b81fe3b1910b0a7836e3de21
Adam Mark <adammark75@gmail.com>
2012-05-22 10:03:21 -0400

    Upgraded to 1.9.3

--- orientation/js/view/VideoView.js
+++ orientation/js/view/VideoView.js
@@ -1,29 +1,35 @@
 function VideoView() {
-    var self = this;
 
     this.init = function () {
-        bc.device.setAutoRotateDirections(["all"]);
+        showLoading();
 
-        self.showLoading();
-
-        self.loadTemplates("../txt/videos.txt", function() {
-            bc.core.getData("videos", renderPlaylist, handleError);
-        });
+        bc.core.getData("videos", handleData, handleError);
     };
 
-    var renderPlaylist = function (data) {
-        var template = self.getTemplate("playlist");
-        var context = { playlists: data.items };
-        var markup = Mark.up(template, context);
+    var handleData = function (data) {
+        renderPlaylist(data);
 
-        document.getElementById("videos").innerHTML = markup;
-
-        self.hideLoading();
+        hideLoading();
     };
 
     var handleError = function (error) {
         console.log(error);
     };
-}
 
-VideoView.prototype = new View();
+    var renderPlaylist = function (data) {
+        var template = bc.templates["video-grid-template"];
+        var context = { playlists: data.items };
+        var markup = Mark.up(template, context);
+
+        document.getElementById("video-grid").innerHTML = markup;
+    };
+
+    var showLoading = function () {
+        document.getElementById("loading").style.opacity = 1;
+    };
+
+    var hideLoading = function () {
+        document.getElementById("loading").style.opacity = 0;
+    };
+
+}
