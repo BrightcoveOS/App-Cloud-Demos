b35510b8a8486f5104a7db87dffa51162beba771
Adam Mark <adammark75@gmail.com>
2012-07-26 18:22:37 -0400

    Updated doc

--- apps-on-tap/javascripts/app.js
+++ apps-on-tap/javascripts/app.js
@@ -1,18 +1,30 @@
+/*
+ * Global functions for this app.
+ */
 var app = {};
 
-// show a "Loading ..." message
+/*
+ * Show a "Loading ..." message (<div id="loading" class="loading">).
+ * See app.css for styles.
+ */
 app.showLoadingMessage = function () {
     document.getElementById("loading").style.opacity = 1;
 };
 
-// hide the "Loading ..." message after a brief delay
+/*
+ * Hide the "Loading ..." message after a brief delay.
+ */
 app.hideLoadingMessage = function() {
     setTimeout(function () {
         document.getElementById("loading").style.opacity = 0;
     }, 300);
 };
 
-// render a Markup.js template given the DOM element ID and template ID
+/*
+ * Render a Markup.js template given the template ID, element ID and context
+ * object. See https://github.com/adammark/Markup.js and
+ * http://blog.brightcove.com/en/2012/05/making-better-markup-markupjs
+ */
 app.renderTemplate = function (elemId, templateId, context) {
     var options = {
         globals: {
@@ -26,15 +38,25 @@ app.renderTemplate = function (elemId, templateId, context) {
     document.getElementById(elemId).innerHTML = markup;
 };
 
-// compare two data feed objects
+/*
+ * Compare two data feeds (JSON objects). For more complex comparison
+ * operations, see bc.utils.isEqual().
+ */
 app.dataChanged = function (feed1, feed2) {
     return JSON.stringify(feed1) !== JSON.stringify(feed2);
 };
 
-// find an object in an array where its value for key equals val
-app.findInDataSet = function (data, key, val) {
+/*
+ * Find an object in an array of objects where the given object property
+ * has the given value.
+ * 
+ * Example:
+ *
+ * var article = app.findInDataSet(articles, "articleId", "12345");
+ */
+app.findInDataSet = function (data, propName, propVal) {
     for (var i = 0; i < data.length; i++) {
-        if (data[i][key] === val) {
+        if (data[i][propName] === propVal) {
             return data[i];
         }
     }
@@ -42,14 +64,18 @@ app.findInDataSet = function (data, key, val) {
     return null;
 };
 
-// remove an image from memory by overwriting the source
+/*
+ * Remove an image from memory by overwriting its source value.
+ */
 app.freeImage = function (img) {
     setTimeout(function () {
         img.src = "../images/blank.png";
     }, 250);
 };
 
-// animate an element to x, y
+/*
+ * Animation an element to position (x, y).
+ */
 app.moveTo = function (elemId, x, y) {
     var elem = document.getElementById(elemId);
     elem.style.setProperty("-webkit-transform", "translate3d(" + x + "px," + y + "px,0px)");
